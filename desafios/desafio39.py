41f8fcb1058bbfb926f21b0479e42b2720ff6471
FilipeRibeiro12 <lipi1702@gmail.com>
2025-02-04 22:36:39 -0300

    Consertando o desafio 39

--- desafios/desafio39.py
+++ desafios/desafio39.py
@@ -3,16 +3,17 @@ id = input('Digite sua idade: ').strip()
 
 if id.isdigit():
     idade = int(id)
-    
-    print('''Qual é o seu sexo ? 
-          digite "1" para masculino e "2" para feminino. ''')
-    
-    sexo = input('').strip
 
+    print('Qual é o seu sexo ? digite "1" para masculino e "2" para feminino.')
+    sexo = int(input(''))
     if sexo == 1:
-        print('Voce se fudeo')
-    
+       print(f'Olá {nome}, você se fudeo')
+    elif sexo == 2:
+        print(f'Olá {nome} Você não tem obrigatoriedade de se alistar')
+    else:
+        print('Por favor, digite uma alternativa valida')
+
 else:
-    print('Digite somente números')
+    print('Por favor, Digite somente seu nome e somente os numeros da sua idade.')
 
 
